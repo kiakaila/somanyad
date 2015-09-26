bd33939b0a93d1b9d07e73f20392309a3dfc212f
Lyman Lai <lyman.lai@yaha.me>
2015-09-26 20:16:24 +0800

    remove empty line

--- controllers/domains/index.js
+++ controllers/domains/index.js
@@ -3,19 +3,12 @@ var emailVerify = require("./api_emailVerify");
 var domains = require("./domains");
 var domains_api = require("./api_domains");
 
-
 exports.api_addForwardEmail = emailVerify.addForwardEmail;
 exports.api_emailVerify = emailVerify.emailVerify;
 exports.api_emailVerifyList = emailVerify.emailVerifyList;
 exports.api_removeEmailVerify = emailVerify.removeEmailVerify;
-
-
 exports.api_cnameVerifyStatus = domains_api.cnameVerifyStatus;
 exports.api_mxVerifyStatus = domains_api.mxVerifyStatus;
-
-
-
-
 exports.home = domains.home;
 exports.setup = domains.setup;
 exports.emails = domains.emails;
