4336670e16075b6a6d2b46408c9fbb35848241e6
Lai Jinyan <ljy080829@gmail.com>
2015-10-09 07:33:30 +0800

    已做:
    	mx 验证生效
    
    待完成:
    
    	邮件转发, 查看是否超出限额
    	 显示用户当前会员情况
    	多语言
    
    	首页
    
    	userOwnerDomain 的 next(err)页面没有处理
    
    	程序启动后, 自动为两小时内有登陆的用户的域名做 cname 以及 mx 验证
    
    	域名设置页面的, 复制功能, 未完成
    	域名设置,提交验证页面, Ajax 化处理
    
    	付费
    		用户的已用额度,
    		超额后, 发送付费提示, 最近10条转发记录(不包含作废地址)

--- controllers/domains/index.js
+++ controllers/domains/index.js
@@ -1,18 +1,21 @@
 
-var emailVerify = require("./api_emailVerify");
+var router = require("express").Router();
+var async = require("async");
+
 var domains = require("./domains");
 var domains_api = require("./api_domains");
 var fee_plan = require("./fee_plan");
 var blacklist = require("./blacklist");
-var router = require("express").Router();
+
+// models
 var Domain = require("../../models/Domain").Domain;
 var EmailVerify = require("../../models/Domain").EmailVerify;
-var userOwnerDomain = require("./middleware").userOwnerDomain;
-var async = require("async");
+
+// middleware
 var m = require('./middleware');
 
+
 router.use(m.locals_domains);
-// router.use(userOwnerDomain);
 // 显示所有域名相关信息
 router.get('/', domains.home);
 
@@ -27,27 +30,31 @@ router.get("/newDomainSetup", domains.newDomainSetup);
 // 添加新域名 -- 完成
 router.get("/newDomainSetup2", domains.newDomainSetup2);
 
+// 检测 cname
+router.get("/cnamelookup", m.userOwnerDomain, domains_api.cnameVerifyStatus)
+// 检测 mx 记录
+router.get("/mxlookup", m.userOwnerDomain, domains_api.mxVerifyStatus)
 
 // 编辑某域名
-router.get("/edit", userOwnerDomain, domains.edit);
+router.get("/edit", m.userOwnerDomain, m.userDomainInfo, domains.edit);
 
 
 // 验证允许转发的邮件地址
 router.get("/emailVerify", domains.emailVerify);
 // 将某个域名的转发邮件修改为另一个邮件地址
-router.post("/edit/forward_email", userOwnerDomain, domains.change_forward_email_post);
+router.post("/edit/forward_email", m.userOwnerDomain, domains.change_forward_email_post);
 
 // 域名删除
-router.get('/deleteDomain', userOwnerDomain, domains.deleteDomain);
-router.post('/deleteDomain', userOwnerDomain, domains.deleteDomain_post);
+router.get('/deleteDomain', m.userOwnerDomain, domains.deleteDomain);
+router.post('/deleteDomain', m.userOwnerDomain, domains.deleteDomain_post);
 
 
 // 为某域名添加黑名单
-router.post("/edit/addBlackList", userOwnerDomain, blacklist.addBlackList_post);
+router.post("/edit/addBlackList", m.userOwnerDomain, blacklist.addBlackList_post);
 // 修改黑名单回退信息
-router.post("/edit/changeBlackItemReplyinfo", userOwnerDomain, blacklist.changeBlackItemReplyinfo_post);
+router.post("/edit/changeBlackItemReplyinfo", m.userOwnerDomain, blacklist.changeBlackItemReplyinfo_post);
 // 删除某条黑名单
-router.get("/edit/removeBlackItem", userOwnerDomain, blacklist.removeBlackItem);
+router.get("/edit/removeBlackItem", m.userOwnerDomain, blacklist.removeBlackItem);
 
 
 // 获取,已用额度
