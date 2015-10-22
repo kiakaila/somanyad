eea7c00d771d84fb7b85b2974e6d1b49df44bef5
Lai Jinyan <ljy080829@gmail.com>
2015-10-22 23:21:45 +0800

    [deploy]  不允许用户转发邮件给自己, 像 gmail 会直接拒绝掉

--- controllers/forward/emailforward.js
+++ controllers/forward/emailforward.js
@@ -21,66 +21,93 @@ function emailForward (mail_from, rcpt_to, cb) {
 	// Check user's domain in db
 	// 检测 to field 邮件地址的 host 是否位于数据库(或者域名)
 
+  // 查看域名是否存在, 且通过验证
+  function existVerifyDomain(done) {
+    Domain.findOne({domain: rcpt_to.host, cnameVerifyStatus: true}, function (err, domain) {
+      err = err || domain == null ? new Error("domain(" + rcpt_to.host + ") not found!") : null;
+      done(err, domain);
+    });
+  }
+
+  // 查看发送目的地(rcpt_to.user @ rcpt_to.host)是否处于废弃地址中
+  function addressWasNotReject(domain, done) {
+    // user: ObjectId,
+    // // blockAddress@domain ==> email address
+    // domain: ObjectId,
+    // blockAddress: String,
+    // replyInfo: String
+    BlackReceiveList.findOne({domain: domain._id, blockAddress: rcpt_to.user}, function (err, blackRecord) {
+      if (blackRecord) {
+        err = new Error(blackRecord.replyInfo);
+        return done(err);
+      }
+      done(err, domain)
+    });
+  }
+
+  // 查看转发目的地记录, 且转发目的地已授权转发
+  function findForwardVerifyAddress(domain, done) {
+    EmailVerify.findOne({_id: domain.forward_email, passVerify: true}, function (err, emailV) {
+      err = err ||
+            emailV == null ? new Error("never found email record") : null;
+      done(err, domain, emailV.email);
+    });
+  }
+
+  // 确保发送者,和转发目的地不是同一个地址
+  function makeSureForwardAddressWasNotEqualSendAddress(domain, address, done) {
+    var lastIndexOfAt = address.lastIndexOf("@");
+    if (lastIndexOfAt != -1) {
+      var addressDomain = address.substring(lastIndexOfAt + 1);
+      if (addressDomain == toHost) {
+        var err = new Error("请不要转发邮件给自己, 有的邮箱会拒绝接收,他发出去的邮件转发给他自己")
+        return done(err)
+      }
+    }
+    done(null, domain, address);
+  }
+
+  // 查看 转发是否超出限额, 并且计数
+  // 如果不是流量包不足, 可以让发送邮件的帮她购买流量包...o^|^o...
+  // 当然,顺便发送一封邮件到用户的转发目的地里...o^|^o...该交钱了,娃
+  function makeSureUserHasEnoughForwardCount(domain, address, done) {
+    var q = {
+      user: domain.user,
+      expireAt: {
+        $gte: m().subtract(1, "days") // 往后延一天
+      }
+    }
+
+    feePlan.find(q).sort({expireAt: 1}).exec(function (err, plans) {
+      if (err) {
+        return done(err);
+      }
+      console.log(plans);
+      for (planIdx in plans) {
+        var plan = plans[planIdx];
+        if (plan.availCount == -1 || plan.usedCount < plan.availCount) {
+          plan.usedCount += 1;
+          return done(null, domain, address);
+        }
+      }
+      return done(new Error("account was not pay fee"))
+    });
+  }
+
+
   async.waterfall([
     // 查看域名是否存在, 且通过验证
-    function (done) {
-      Domain.findOne({domain: rcpt_to.host, cnameVerifyStatus: true}, function (err, domain) {
-        err = err || domain == null ? new Error("domain(" + rcpt_to.host + ") not found!") : null;
-        done(err, domain);
-      });
-    },
+    existVerifyDomain,
     // 查看发送目的地(rcpt_to.user @ rcpt_to.host)是否处于废弃地址中
-    function (domain, done) {
-
-      // user: ObjectId,
-      // // blockAddress@domain ==> email address
-      // domain: ObjectId,
-      // blockAddress: String,
-      // replyInfo: String
-      BlackReceiveList.findOne({domain: domain._id, blockAddress: rcpt_to.user}, function (err, blackRecord) {
-        if (blackRecord) {
-          err = new Error(blackRecord.replyInfo);
-          return done(err);
-        }
-        done(err, domain)
-      });
-    },
+    addressWasNotReject,
     // 查看转发目的地记录, 且转发目的地已授权转发
-    function (domain, done) {
-      EmailVerify.findOne({_id: domain.forward_email, passVerify: true}, function (err, emailV) {
-        err = err ||
-              emailV == null ? new Error("never found email record") : null;
-        done(err, domain, emailV.email);
-      });
-    },
+    findForwardVerifyAddress,
+    // 确保发送者,和转发目的地不是同一个地址
+    makeSureForwardAddressWasNotEqualSendAddress,
     // 查看 转发是否超出限额, 并且计数
     // 如果不是流量包不足, 可以让发送邮件的帮她购买流量包...o^|^o...
     // 当然,顺便发送一封邮件到用户的转发目的地里...o^|^o...该交钱了,娃
-    function (domain, address, done) {
-      // 还没设计, 先让他可以转发...
-
-      var q = {
-        user: domain.user,
-        expireAt: {
-          $gte: m().subtract(1, "days") // 往后延一天
-        }
-      }
-
-      feePlan.find(q).sort({expireAt: 1}).exec(function (err, plans) {
-        if (err) {
-          return done(err);
-        }
-        console.log(plans);
-        for (planIdx in plans) {
-          var plan = plans[planIdx];
-          if (plan.availCount == -1 || plan.usedCount < plan.availCount) {
-            plan.usedCount += 1;
-            return done(null, domain, address);
-          }
-        }
-        return done(new Error("account was not pay fee"))
-      });
-    }
+    makeSureUserHasEnoughForwardCount
   ], function (err, domain, address) {
     cb(err, address || null);
   });
